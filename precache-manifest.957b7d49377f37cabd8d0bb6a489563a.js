88cbf0ee952cff1e861d4851809083abb877a32c
strizzzer <vitya752@ukr.net>
2019-09-20 12:51:15 +0300

    Updates

---
+++ precache-manifest.957b7d49377f37cabd8d0bb6a489563a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2574554e56392c600d6da13b38139dae",
+    "url": "/todoList/index.html"
+  },
+  {
+    "revision": "fcf53be3523584d858ff",
+    "url": "/todoList/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c659b731bbc06d91b85a",
+    "url": "/todoList/static/css/main.61496664.chunk.css"
+  },
+  {
+    "revision": "fcf53be3523584d858ff",
+    "url": "/todoList/static/js/2.258159c6.chunk.js"
+  },
+  {
+    "revision": "c659b731bbc06d91b85a",
+    "url": "/todoList/static/js/main.2f3fb54f.chunk.js"
+  },
+  {
+    "revision": "626be551c5e1c87fa8dc",
+    "url": "/todoList/static/js/runtime~main.caf1dcf7.js"
+  },
+  {
+    "revision": "e94190b3496fb8605799973ff4743082",
+    "url": "/todoList/static/media/paris.e94190b3.jpg"
+  }
+]);
