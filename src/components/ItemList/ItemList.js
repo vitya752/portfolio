9d9451d04157c415dd5aecfaf73a3ba2fc21e910
vitya752 <vitya752@ukr.net>
2019-09-20 08:45:38 +0300

    refactoring

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -8,30 +8,29 @@ const ItemList = ({ data, important, done, onToggleImportant, onToggleDone, onIt
 
     const importantItem = important ? 'active' : null;
     const doneItem = done ? 'doneItem' : null;
+    const buttons = [
+        { classNames: "btn btn-primary", onClickFunction: onToggleImportant, fontAwesomeIcon: faExclamation },
+        { classNames: "btn btn-success btn-done", onClickFunction: onToggleDone, fontAwesomeIcon: faCheckSquare },
+        { classNames: "btn btn-danger", onClickFunction: onItemDelete, fontAwesomeIcon: faTrash }
+    ]
+    
+    const renderButtons = buttons.map((item) => {
+        return(
+            <button
+                type="button"
+                className={item.classNames}
+                onClick={item.onClickFunction} >
+                <FontAwesomeIcon icon={item.fontAwesomeIcon} />
+            </button>
+        );
+    });
 
     return(
         <li
             className={`list-group-item list-group-item--custom ${importantItem} ${doneItem}`} >
             { data }
             <div className="float-right buttons-wrap">
-                <button
-                    type="button"
-                    className="btn btn-primary"
-                    onClick={onToggleImportant} >
-                    <FontAwesomeIcon icon={faExclamation} />
-                </button>
-                <button
-                    type="button"
-                    className="btn btn-success btn-done"
-                    onClick={onToggleDone} >
-                    <FontAwesomeIcon icon={faCheckSquare} />
-                </button>
-                <button 
-                    className="btn btn-danger" 
-                    type="button"
-                    onClick={onItemDelete} >
-                    <FontAwesomeIcon icon={faTrash} />
-                </button>
+                {renderButtons}
             </div>
         </li>
     );
